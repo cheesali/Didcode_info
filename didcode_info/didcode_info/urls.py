383188f7e891c8603b1c711b4cfb83d4ff4c891b
Alina <70215718+cheesali@users.noreply.github.com>
2024-06-20 16:59:29 +0200

    Add files via upload

--- didcode_info/didcode_info/urls.py
+++ didcode_info/didcode_info/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from hello import views
 
 urlpatterns = [
-    path("", include("hello.urls"))
+    path("", include("hello.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
--- didcode_info/hello/views.py
+++ didcode_info/hello/views.py
@@ -16,31 +16,32 @@ def get_didcode_info(request, DIDCODE):
     data = ""
     with connections['didcode_db'].cursor() as cursor:
         cursor.execute(f"""
-                        SELECT  u.DIDCODE,
-                                u.PART_NUMBER,
-                                u.QTY,
-                                u.LOT_ID,
-                                u.INSERTION_DATETIME,
-                                u.INSERTION_USERID,
-                                u.UPDATE_DATETIME,
-                                u.UPDATE_USER_ID,
-                                u.WAREHOUSE
+                        SELECT u.ID,  
+                               u.DIDCODE,
+                               u.PART_NUMBER,
+                               u.QTY,
+                               u.LOT_ID,
+                               u.INSERTION_DATETIME,
+                               u.INSERTION_USERID,
+                               u.UPDATE_DATETIME,
+                               u.UPDATE_USER_ID,
+                               u.WAREHOUSE
                         FROM users u
                         WHERE u.DIDCODE = '{DIDCODE}'
                         """)
         data = cursor.fetchall()
         json_kek = {
-            
-            "DIDCODE": data[0][0],
-            "PART_NUMBER": data[0][1],
-            "QTY": data[0][2],
-            "LOT_ID": data[0][3],
-            "INSERTION_DATETIME": data[0][4],
-            "INSERTION_USERID": data[0][5],
-            "UPDATE_DATETIME": data[0][6],
-            "UPDATE_USER_ID": data[0][7],
-            "WAREHOUSE": data[0][8],
-
+            "ID": data[0][0], 
+            "DIDCODE": data[0][1],
+            "PART_NUMBER": data[0][2],
+            "QTY": data[0][3],
+            "LOT_ID": data[0][4],
+            "INSERTION_DATETIME": data[0][5],
+            "INSERTION_USERID": data[0][6],
+            "UPDATE_DATETIME": data[0][7],
+            "UPDATE_USER_ID": data[0][8],
+            "WAREHOUSE": data[0][9], 
         }
     return JsonResponse(json_kek)
+
     
